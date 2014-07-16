e1684960340942c68a156afb67f76174b286352a
sxend <arimitsu.k+github@gmail.com>
2014-07-16 21:42:44 +0900

    change to gradle project

--- src/main/scala/arimitsu/sf/akka/cqlclient/Authentication.scala
+++
@@ -1,8 +0,0 @@
-package arimitsu.sf.akka.cqlclient
-
-/**
- * Created by sxend on 2014/06/10.
- */
-case class Authentication(id: String) {
-
-}
--- src/main/scala/arimitsu/sf/akka/cqlclient/UnhandledException.scala
+++
@@ -1,8 +0,0 @@
-package arimitsu.sf.akka.cqlclient
-
-/**
- * Created by sxend on 14/06/28.
- */
-class UnhandledException(e: Throwable) extends RuntimeException {
-
-}
