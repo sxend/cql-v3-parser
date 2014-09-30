ba046c701d58e5876b06e2c9cc58abe2602230dc
sxend <arimitsu.k+github@gmail.com>
2014-09-30 20:57:54 +0900

    add Flags document

--- src/main/java/arimitsu/sf/cql/v3/Flags.java
+++ src/main/java/arimitsu/sf/cql/v3/Flags.java
@@ -1,25 +1,44 @@
 package arimitsu.sf.cql.v3;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sxend on 14/06/04.
+ * Flags applying to this frame. The flags have the following meaning
  */
 public enum Flags {
 
     NONE(0x00),
+    /**
+     * Compression flag.
+     */
     COMPRESSION(0x01),
+    /**
+     * Tracing flag.
+     */
     TRACING(0x02),
-    BOTH(0x03),;
+    /**
+     * both positive
+     */
+    BOTH(COMPRESSION.value | TRACING.value),;
 
     public final byte value;
+
+    private static final Map<Byte, Flags> FLAGS_MAP;
+
+    static {
+        Map<Byte, Flags> map = new HashMap<>();
+
+        FLAGS_MAP = Collections.unmodifiableMap(map);
+    }
 
     Flags(int value) {
         this.value = (byte) value;
     }
 
     public static Flags valueOf(byte value) {
-        for (Flags v : values()) {
-            if (v.value == value) return v;
-        }
-        throw new RuntimeException("invalid value");
+        return FLAGS_MAP.get(value);
     }
 }
