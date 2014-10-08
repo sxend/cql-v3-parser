f523b7534d884dd5d4e2b05e10c16db3555b007c
sxend <arimitsu.k+github@gmail.com>
2014-10-08 12:03:28 +0900

    remove type parameter

--- src/main/java/arimitsu/sf/cql/v3/message/Request.java
+++ src/main/java/arimitsu/sf/cql/v3/message/Request.java
@@ -4,7 +4,7 @@ package arimitsu.sf.cql.v3.message;
 /**
  * Created by sxend on 14/06/07.
  */
-public abstract class Request<R> {
+public abstract class Request {
     protected static final byte[] EMPTY_BODY = new byte[0];
 
     public abstract byte[] toBody();
