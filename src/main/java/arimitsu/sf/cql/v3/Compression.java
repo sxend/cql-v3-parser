49d729447add29f8883960c98aae18cae61706da
sxend <arimitsu.k+github@gmail.com>
2014-10-09 11:51:11 +0900

    add compressor

--- src/main/java/arimitsu/sf/cql/v3/Compression.java
+++ src/main/java/arimitsu/sf/cql/v3/Compression.java
@@ -1,19 +1,26 @@
 package arimitsu.sf.cql.v3;
 
 
+import arimitsu.sf.cql.v3.compressor.LZ4Compressor;
+import arimitsu.sf.cql.v3.compressor.NoneCompressor;
+import arimitsu.sf.cql.v3.compressor.SnappyCompressor;
+
 /**
  * Created by sxend on 14/06/08.
  */
 public enum Compression {
 
-    NONE(""),
-    LZ4("lz4"),
-    SNAPPY("snappy"),;
+    NONE("", new NoneCompressor()),
+    LZ4("lz4", new LZ4Compressor()),
+    SNAPPY("snappy", new SnappyCompressor()),;
 
     public final String name;
 
-    Compression(String name) {
+    public final Compressor compressor;
+
+    Compression(String name, Compressor compressor) {
         this.name = name;
+        this.compressor = compressor;
     }
 
     public static Compression valueOf(byte[] name) {
